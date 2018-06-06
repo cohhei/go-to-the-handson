13df07b2c460e8d1213e88a1b7ad68bfdca59c07
Kohei Kimura <kohei.kimura@nulab-inc.com>
2018-06-06 11:43:45 +0900

    update-02: add new endpoint to return a JSON data

--- 02/main/server.go
+++ 02/main/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,6 +10,26 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %q", r.URL.Path)
+	})
+
+	http.HandleFunc("/json", func(w http.ResponseWriter, r *http.Request) {
+		data := struct {
+			ID          int    `json:"id"`
+			Name        string `json:"name"`
+			MailAddress string `json:"mail_address"`
+			Options     string `json:"options,omitempty"`
+		}{
+			ID:   1,
+			Name: "my_name",
+		}
+
+		b, err := json.Marshal(data)
+		if err != nil {
+			fmt.Fprint(w, err)
+			return
+		}
+
+		w.Write(b)
 	})
 
 	fmt.Println("http://localhost:8080")
